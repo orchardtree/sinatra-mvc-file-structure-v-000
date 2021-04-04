94bc94b223ab8ed6d78dadb61dba1b3ce759e076
Jesse Wenger <wengerj@gmail.com>
2021-04-04 01:40:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,22 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  
+
 end
+
+  class Dog 
+    @@all = []
+    attr_accessor :name, :breed, :age
+    
+    def initialize(name, breed, age)
+      @name = name 
+      @breed = breed 
+      @age = age
+      @@all << self
+    end
+    
+    def self.all 
+      @@all
+    end
+  end
